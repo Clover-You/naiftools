db7439662bc14d149c785686702a6d7096a6f906
Clover You <2621869236@qq.com>
2025-01-15 14:21:25 +0800

    refactor: rename typeofs result type

--- packages/core/typeofs/index.ts
+++ packages/core/typeofs/index.ts
@@ -1,6 +1,6 @@
-type Typeof = 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'null' | 'object' | 'array' | 'function'
+export type TypeOfResult = 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'null' | 'object' | 'array' | 'function'
 
-export function typeofs(ukn: unknown): Typeof {
+export function typeofs(ukn: unknown): TypeOfResult {
   if (ukn === null)
     return 'null'
   if (Array.isArray(ukn))
