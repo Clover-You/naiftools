65d3c12de4775a501bc17f26204487a687d54cd3
Clover You <2621869236@qq.com>
2025-01-15 11:14:22 +0800

    chore: rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,71 @@
 import type { RollupOptions } from 'rollup'
+import type { Options as ESBuildOptions } from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
 
-const configs: RollupOptions[] = []
+const pluginDts = dts()
 
-export function createRollupConfig() {
-  const functionNames = ['index']
+const iifeName = 'NaifTools'
 
-  for (const fn of functionNames) {
-    const input = fn === 'index'
-      ? `index.ts`
-      : `${fn}/index.ts`
+/**
+ * This function is adapted from the open-source project [VueUse]
+ * Source: [https://github.com/vueuse/vueuse/blob/d0350356eea3065d968ef42f97badff577e539fd/rollup.config.ts#L22](VueUse)
+ * License: MIT
+ */
+function esbuildMinifer(options: ESBuildOptions) {
+  const { renderChunk } = esbuild(options)
 
-    configs.push({
-      input,
-    })
+  return {
+    name: 'esbuild-minifer',
+    renderChunk,
   }
-
-  return configs
+}
+export function createRollupConfig() {
+  return [
+    {
+      input: 'index.ts',
+      output: [
+        {
+          file: 'index.mjs',
+          format: 'es',
+        },
+        {
+          file: 'index.cjs',
+          format: 'cjs',
+        },
+        {
+          file: 'index.iife.js',
+          format: 'iife',
+          name: iifeName,
+          extend: true,
+          plugins: [],
+        },
+        {
+          file: 'index.iife.min.js',
+          format: 'iife',
+          name: iifeName,
+          extend: true,
+          plugins: [
+            esbuildMinifer({
+              minify: true,
+            }),
+          ],
+        },
+      ],
+      plugins: [
+        esbuild({
+          target: 'es2018',
+        }),
+      ],
+    },
+    {
+      input: 'index.ts',
+      output: [
+        { file: `index.d.cts` },
+        { file: `index.d.mts` },
+        { file: `index.d.ts` },
+      ],
+      plugins: [pluginDts],
+    },
+  ] satisfies RollupOptions[]
 }
