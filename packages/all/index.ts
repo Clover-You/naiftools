28a20f5b6f305a87fd9aeb5a5ce47673baa0c144
Clover You <2621869236@qq.com>
2025-02-13 10:50:04 +0800

    feat: array package

--- packages/all/index.ts
+++ packages/all/index.ts
@@ -1,6 +1,6 @@
 import * as core from '@naiftools/core'
-import * as strings from '@naiftools/string'
 
-export { strings }
+export * as arrays from '@naiftools/array'
+export * as strings from '@naiftools/string'
 
 export default core
